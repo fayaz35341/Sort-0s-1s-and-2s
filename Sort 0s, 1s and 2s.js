024a3caa43e503b90d778c448e04a52e7660a3da
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2026-01-01 11:07:12 +0530

    Create Sort 0s, 1s and 2s.js

---
+++ Sort 0s, 1s and 2s.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} arr
+ * @returns {void}
+ */
+
+class Solution {
+    sort012(arr) {
+        // code here
+        let low=0
+        let mid=0
+        let high=arr.length-1
+        while (mid<=high) {
+            if (arr[mid]===0) {
+                let temp=arr[low]
+                arr[low]=arr[mid]
+                arr[mid]=temp
+                low++
+                mid++
+            }
+            else if (arr[mid]===1){
+                mid++
+            }
+            else{
+                let temp=arr[mid]
+                arr[mid]=arr[high]
+                arr[high]=temp
+                high--
+            }
+        }    
+    }
+}
